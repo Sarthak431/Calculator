92cfed100ef1d11f2960d691ad791cf9cda9af26
Sarthak431 <136489174+Sarthak431@users.noreply.github.com>
2023-07-29 15:15:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("result").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("result").value = displayValue;
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("result").value = displayValue;
+    } catch (error) {
+        document.getElementById("result").value = "Error";
+    }
+}
+
+function operate(operator) {
+    displayValue += operator;
+    document.getElementById("result").value = displayValue;
+}
